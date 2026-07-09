8b14e3c0561695a6aac8d773c719681d1b79b265
leocasttro <leocasttro01@icloud.com>
2026-07-09 13:15:38 -0300

    chorme: configuração no app e serve adptado ao sistema

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { HttpError } from './shared/presentation/errors/HttpError';
+import { errorHandler } from './shared/presentation/errors/errorHandler';
 
 export const app = express();
 
@@ -7,5 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/health', (_request, response) => {
-    return response.status(200).json({ status: 'ok' });
+  return response.status(200).json({ status: 'ok' });
 });
+
+app.use(() => {
+  throw new HttpError('Rota não encontrada.', 404);
+});
+
+app.use(errorHandler);
